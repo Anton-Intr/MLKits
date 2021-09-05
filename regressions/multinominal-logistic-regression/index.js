f7712fc8020bc44fd8ea2658a6aaf38002c580e5
Anton-Intr <88795567+Anton-Intr@users.noreply.github.com>
2021-09-05 11:27:16 +0300

    Lecture 139

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -6,26 +6,34 @@ const plot = require('node-remote-plot')
 
 const { features, labels, testFeatures, testLabels } = loadCSV('C:\\Users\\Anton\\MLCourse\\MLKits\\regressions\\logistic-regression\\cars.csv', {
     dataColumns: ['horsepower', 'displacement', 'weight'],
-    labelColumns: ['passedemissions'],
+    labelColumns: ['mpg'],
     shuffle: true,
     splitTest: 50,
     converters: {
-        passedemissions: (value) => {
-            return value === 'TRUE' ? 1 : 0
+        mpg: (value) => {
+            const mpg = parseFloat(value)
+            if (mpg < 15) {
+                return [1, 0, 0]
+            } else if (mpg < 30) {
+                return [0, 1, 0]
+            } else {
+                return [0, 0, 1]
+            }
         }
     }
-})
-// console.log(labels)
+}
+)
+console.log(labels)
 
-const regression = new LogisticRegression(features, labels, {
-    learningRate: 0.5,
-    iterations: 100,
-    batchSize: 10,
-    decisionBoundary: 0.5
-})
+// const regression = new LogisticRegression(features, labels, {
+//     learningRate: 0.5,
+//     iterations: 100,
+//     batchSize: 10,
+//     decisionBoundary: 0.5
+// })
 
-regression.train()
-console.log(regression.test(testFeatures, testLabels))
-plot({
-    x: regression.costHistory.reverse()
-})
+// regression.train()
+// console.log(regression.test(testFeatures, testLabels))
+// plot({
+//     x: regression.costHistory.reverse()
+// })
